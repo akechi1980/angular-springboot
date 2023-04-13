abba5703e031421fa261037ccebee34e2b29e661
程序员WO的代码库 <akechi1980@gmail.com>
2023-04-13 17:24:19 +0000

    tailwind.css 追加

--- ng-src/app/pages/company-list/company-list.component.ts
+++ ng-src/app/pages/company-list/company-list.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { UsersService } from '../user-list/users.service';
 
 @Component({
   selector: 'app-company-list',
   templateUrl: './company-list.component.html'
 })
 export class CompanyListComponent {
+
+  data: any = {};
+
+  constructor(private mUsersService: UsersService) { }
+
+  ngOnInit(): void {
+    this.mUsersService.getData().subscribe(data => {
+      console.log(data);
+      this.data = data;
+    });
+  }
 
 }
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./ng-src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
