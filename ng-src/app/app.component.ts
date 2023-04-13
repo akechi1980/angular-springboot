e5b192946497cc053e43e160e7d203bead79c9fb
程序员WO的代码库 <akechi1980@gmail.com>
2023-04-13 09:00:44 +0000

    +++

--- ng-src/app/app.component.ts
+++ ng-src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { environment } from './../environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-first-angular';
+  title = '!!Test!!';
+
+  constructor() {
+    console.log(environment.production); // Logs false for development environment
+  }
+
 }
--- src/main/java/com/exp/demo/apidemo/dao/memory/User.java
+++ src/main/java/com/exp/demo/apidemo/dao/memory/User.java
@@ -1,6 +1,5 @@
 package com.exp.demo.apidemo.dao.memory;
 
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,7 +7,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
-
 
 @lombok.AllArgsConstructor
 @lombok.NoArgsConstructor
@@ -21,6 +19,12 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    
+    // ユーザ名
     private String name;
+    // パスワード
+    private String password;
+    // 年齢
+    private int age;
+    // メールアドレス
+    private String email;
 }
